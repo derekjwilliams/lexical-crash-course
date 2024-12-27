89bef300717ceb7bea5f5593c0886590d3f1dd5f
derek williams <derek61@gmail.com>
2024-12-27 14:37:07 -0700

    add undo and redo, see video timestamp 830 seconds

--- src/app/components/LexicalToolbar.tsx
+++ src/app/components/LexicalToolbar.tsx
@@ -1,11 +1,28 @@
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
-import { $getSelection, $isRangeSelection, FORMAT_TEXT_COMMAND } from 'lexical'
-import React, { useCallback, useEffect, useState } from 'react'
+import { mergeRegister } from '@lexical/utils'
+import {
+  $getSelection,
+  $isRangeSelection,
+  CAN_REDO_COMMAND,
+  CAN_UNDO_COMMAND,
+  FORMAT_TEXT_COMMAND,
+  REDO_COMMAND,
+  SELECTION_CHANGE_COMMAND,
+  UNDO_COMMAND,
+} from 'lexical'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+
+const LowPriority = 1
 
 export default function LexicalToolbar() {
   const [editor] = useLexicalComposerContext()
+  const toolbarRef = useRef(null)
+  const [canUndo, setCanUndo] = useState(false)
+  const [canRedo, setCanRedo] = useState(false)
   const [isBold, setIsBold] = useState(false)
   const [isItalic, setIsItalic] = useState(false)
+  const [isUnderline, setIsUnderline] = useState(false)
+  const [isStrikethrough, setIsStrikethrough] = useState(false)
 
   const $updateToolbar = useCallback(() => {
     const selection = $getSelection()
@@ -16,26 +33,69 @@ export default function LexicalToolbar() {
   }, [])
 
   useEffect(() => {
-    editor.registerUpdateListener(({ editorState }) => {
-      editorState.read(() => $updateToolbar())
-    })
+    return mergeRegister(
+      editor.registerUpdateListener(({ editorState }) => {
+        editorState.read(() => {
+          $updateToolbar()
+        })
+      }),
+      editor.registerCommand(
+        SELECTION_CHANGE_COMMAND,
+        (_payload, _newEditor) => {
+          $updateToolbar()
+          return false
+        },
+        LowPriority
+      ),
+      editor.registerCommand(
+        CAN_UNDO_COMMAND,
+        (payload) => {
+          setCanUndo(payload)
+          return false
+        },
+        LowPriority
+      ),
+      editor.registerCommand(
+        CAN_REDO_COMMAND,
+        (payload) => {
+          setCanRedo(payload)
+          return false
+        },
+        LowPriority
+      )
+    )
   }, [editor, $updateToolbar])
 
   return (
-    <div className='space-x-5'>
+    <div className='space-x-5' ref={toolbarRef}>
       <button
         onClick={() => {
           editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'bold')
-        }}>
+        }}
+        className={`size-8 ${isBold ? 'bg-gray-200' : ''}`}>
         B
       </button>
       <button
-        className='italic'
         onClick={() => {
           editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'italic')
-        }}>
+        }}
+        className={`italic size-8 ${isItalic ? 'bg-gray-200' : ''}`}>
         i
       </button>
+      <button
+        disabled={!canUndo}
+        onClick={() => {
+          editor.dispatchCommand(UNDO_COMMAND, undefined)
+        }}
+        className='toolbar-item undo'
+        aria-label='Undo'></button>
+      <button
+        disabled={!canRedo}
+        onClick={() => {
+          editor.dispatchCommand(REDO_COMMAND, undefined)
+        }}
+        className='toolbar-item redo'
+        aria-label='Redo'></button>
     </div>
   )
 }
